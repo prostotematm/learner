136233b4d75c32e7ab602470005ad2a24df79117
prostotematm <45317083+prostotematm@users.noreply.github.com>
2024-11-02 17:44:57 +0500

    Add files via upload

---
+++ type.py
@@ -0,0 +1,17 @@
+#1st program
+print('1st task')
+print(9**0.5*5 , '\n')
+
+#2nd program
+print('2ns task')
+print(9.99 > 9.98 and 1000 != 1000.1 , '\n')
+
+#3rd program
+print('3rd task')
+print(2*2+2)
+print(2*(2+2))
+print(2*(2+2) == (2*2+2) , '\n')
+
+#4th program
+print('4th task')
+print(int(float('123.456') * 10) % 10)
