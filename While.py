2f97c3e4cb610f0f8a571eaaf9578b218927b55c
prostotematm <45317083+prostotematm@users.noreply.github.com>
2024-11-26 15:06:02 +0500

    Add files via upload

---
+++ While.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+n = 0
+while n < len(my_list):
+     if my_list[n] < 0:
+          break;
+     else:
+          print(my_list[n])
+          n += 1
+          continue
+
+
+
+
