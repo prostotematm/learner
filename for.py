f1274a60050898984122a08e02556016a7a19933
prostotematm <45317083+prostotematm@users.noreply.github.com>
2024-11-26 23:09:31 +0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,34 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = True
+is_not_prime = False
+
+for i in numbers:
+    if i < 2:
+        continue
+    if i == 2:
+        primes.append(i)
+        continue
+
+    if i % 2 == 0:
+        not_primes.append(i)
+        continue
+
+        for j in range(3, int(j ** 0.5) + 1, 2):
+            if j % i == 0:
+                is_prime = False
+                break
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print("Простые числа: ", primes)
+print("Остальные числа: ", not_primes)
+
+
+
+
+
+
