8597314fcd0d0714c4fbdd5d62f2b67af2596d2b
prostotematm <45317083+prostotematm@users.noreply.github.com>
2024-12-01 12:35:01 +0500

    Update While.py

--- While.py
+++ While.py
@@ -2,7 +2,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 n = 0
 while n < len(my_list):
-     if my_list[n] < 0:
+     if my_list[n] <= 0:
           break
      else:
           print(my_list[n])
